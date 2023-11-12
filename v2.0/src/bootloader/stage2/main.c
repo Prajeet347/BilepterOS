e7d91899e6481be4dbc43bf02bd4eee8cbdd1a88
Prattay Sarkar <orginalprattaysarkar@gmail.com>
2023-11-12 16:17:12 +0530

    ep 6 & im running out of ideas

--- v2.0/src/bootloader/stage2/main.c
+++ v2.0/src/bootloader/stage2/main.c
@@ -1,13 +1,55 @@
 #include "stdint.h"
 #include "stdio.h"
+#include "disk.h"
+#include "fat.h"
+
+void far* g_data = (void far*)0x00500200;
 
 void _cdecl cstart_(uint16_t bootDrive)
 {
-    puts(   "\r\nBilepterOS V1.0  Copyright (C) 2023 Prattay Sarkar\r\n\n"
-            "This program comes with ABSOLUTELY NO WARRANTY\r\n"
-            "For More Details, Visit https://www.gnu.org/licenses/gpl-3.0.en.html\r\n"
-            "This is free software, and you are welcome to redistribute it.\r\n\n"
-    );
-    printf("Formatted %% %s", "This!\r\n");
+    DISK disk;
+    if (!DISK_Initialize(&disk, bootDrive))
+    {
+        printf("Disk init error\r\n");
+        goto end;
+    }
+
+    DISK_ReadSectors(&disk, 19, 1, g_data);
+
+    if (!FAT_Initialize(&disk))
+    {
+        printf("FAT init error\r\n");
+        goto end;
+    }
+
+    // browse files in root
+    FAT_File far* fd = FAT_Open(&disk, "/");
+    FAT_DirectoryEntry entry;
+    int i = 0;
+    while (FAT_ReadEntry(&disk, fd, &entry) && i++ < 5)
+    {
+        printf("  ");
+        for (int i = 0; i < 11; i++)
+            putc(entry.Name[i]);
+        printf("\r\n");
+    }
+    FAT_Close(fd);
+
+    // read test.txt
+    char buffer[100];
+    uint32_t read;
+    fd = FAT_Open(&disk, "mydir/test.txt");
+    while ((read = FAT_Read(&disk, fd, sizeof(buffer), buffer)))
+    {
+        for (uint32_t i = 0; i < read; i++)
+        {
+            if (buffer[i] == '\n')
+                putc('\r');
+            putc(buffer[i]);
+        }
+    }
+    FAT_Close(fd);
+
+end:
     for (;;);
 }
